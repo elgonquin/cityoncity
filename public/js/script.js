ecd7172bbf9e2adc7d6bc1c77497db5516032759
Simon Saint <ultimatesaint@gmail.com>
2013-05-07 21:44:33 +0100

    CSS Complete

--- public/js/script.js
+++ public/js/script.js
@@ -7,7 +7,7 @@ console.log(str);
 $("li a").each(function() {
   if (str.indexOf(this.href.toLowerCase()) > -1) {
     $("li.highlight").removeClass("highlight");
-    $(this).parent().addClass("highlight");
+    $(this).addClass("highlight");
     }
  });
 
